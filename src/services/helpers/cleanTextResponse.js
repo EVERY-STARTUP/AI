da9f0d17ad1c780700703b8137cbb08dd74c2326
misdarmanto <misdarmanto1922@gmail.com>
2025-10-24 10:33:50 +0700

    enhance: remove quote

--- src/services/helpers/cleanTextResponse.js
+++ src/services/helpers/cleanTextResponse.js
@@ -1,18 +1,21 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
+'use strict';
+Object.defineProperty(exports, '__esModule', { value: true });
 exports.cleanLLMResponse = cleanLLMResponse;
 function cleanLLMResponse(text) {
-    return (text
-        // Hapus karakter newline literal seperti \n atau \\n
-        .replace(/\\n+/g, ' ')
-        // Ubah \" menjadi "
-        .replace(/\\"/g, '"')
-        // Hapus karakter backslash ganda (\\)
-        .replace(/\\\\/g, '')
-        // Hapus tanda * dan -
-        .replace(/[*-]+/g, '')
-        // Ganti spasi ganda menjadi satu
-        .replace(/\s{2,}/g, ' ')
-        // Hilangkan spasi di awal dan akhir teks
-        .trim());
+    return (
+        text
+            .replace(/\\n+/g, ' ')
+            // Ubah \" menjadi "
+            .replace(/\\"/g, '"')
+            // Hapus karakter backslash ganda (\\)
+            .replace(/\\\\/g, '')
+            // Hapus tanda * dan -
+            .replace(/[*-]+/g, '')
+            // Hapus tanda kutip hanya di awal dan akhir teks
+            .replace(/^"(.*)"$/, '$1')
+            // Ganti spasi ganda menjadi satu
+            .replace(/\s{2,}/g, ' ')
+            // Hilangkan spasi di awal dan akhir teks
+            .trim()
+    );
 }
